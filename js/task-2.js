2ee889762aef8cbdaf23e6b0ebd2ec3ce21ce7ed
Samir Sharif <samirsharif6969@gmail.com>
2025-06-30 20:27:45 -0400

    new commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,40 +1,18 @@
-function calcAverageCalories(days) {
-    if (days.length === 0) {
-        return 0;
-    }
-    let total = 0;
-    for (const day of days) {
-        total += day.calories;
-    }
-
-    return total / days.length;
-}
+// Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
 
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
+// перший параметр users — масив об’єктів користувачів
+// другий параметр friendName — ім’я друга для пошуку.
+// Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName. Друзі кожного користувача зберігаються у властивості friends. Якщо користувачів, у яких є такий друг немає, то функція має повернути порожній масив.
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
 
-console.log(
-  calcAverageCalories([])
-); // 0
+
+// Поради:
+
+// Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
+// Використовуй метод includes() для перевірки, чи масив friends містить friendName.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+
+const getUsersWithFriend = (users, friendName) =>
+  users.filter(user => user.friends.includes(friendName));
